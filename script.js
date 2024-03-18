fb8207422bb27442423350e6816e8a24f7d3082f
tapuchandramojumder <mojumdertapu@gmail.com>
2024-03-18 22:13:01 +0600

    responsiveness done  for 600px

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ elemArr.forEach(elem => {
         fixedIamge.style.backgroundImage = `url(${image})`;
     })
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+  });
+
+
+
+let menu = document.querySelector('nav h3')
+let fullScreen = document.querySelector("#full-screen")
+let logo = document.querySelector("#nav-part1 img")
+var flag = 0;// Ensure flag is initialized to 0
+
+menu.addEventListener("click", function() {
+    if (flag === 0) {
+        console.log('first clicked');
+        fullScreen.style.top = 0;
+        logo.style.opacity = 0;
+        flag = 1;
+    } else {
+        console.log('second clicked');
+        fullScreen.style.top = "-100%";
+        logo.style.opacity = 1;
+        flag = 0;
+    }
+});
+
+
+
+let loader = document.querySelector("#loader")
+
+setTimeout(function(){
+    loader.style.top='-100%'
+},4000)
